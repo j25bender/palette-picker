373e03712a49a166a1cc0842b7fd83d1f93a799e
Jeff Bender <j25bender@gmail.com>
2018-04-27 11:43:41 -0600

    centered saved palettes row wrapped colors

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -20,19 +20,21 @@ $(window).keypress((e) => {
     !$(document.activeElement).is('#title-input') &&
     !$(document.activeElement).is('#save-title')) {
     setRandomPalette();
+    e.preventDefault();
+    $(window).scrollTop(0);
     } 
 });
 
 const setRandomPalette = () => {
   palette = {};
   for(let i = 1; i <= 5; i++) {
-    if(!$(`#poly${[i]}`).hasClass('locked')) {
+    if(!$(`#poly${i}`).hasClass('locked')) {
       const elementColor = randomColor();
-      $(`#poly${[i]}`).css('fill', elementColor);
-      palette[`color${[i]}`] = elementColor;
+      $(`#poly${i}`).css('fill', elementColor);
+      palette[`color${i}`] = elementColor;
     } else {
-      const lockedColor = $(`#poly${[i]}`).css('fill');
-      palette[`color${[i]}`] = lockedColor;   
+      const lockedColor = $(`#poly${i}`).css('fill');
+      palette[`color${i}`] = lockedColor;   
     }
   }
   paletteState.palette = palette;
@@ -55,6 +57,7 @@ $('#save-title').click( async (e) => {
   $('#title-input').val('');
 
   if(!$('#select-options').children().text().includes(title)) {
+    $('#select-options').append(`<option>${title}</option>`);
     await fetch('/api/v1/projects', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
@@ -70,18 +73,24 @@ $('#save-title').click( async (e) => {
 $('#save-palette').click( async (e) => {
   createPalettes(paletteState.palette)
   const paletteName = $('#palette-input').val();
-  const projectId = $('#select-options').val();
   $('#palette-input').val('');
+  
+  const projectId = $('#select-options').val();
+  const projectTitle = '.' + $('#select-options').text().trim();
 
-  await fetch('/api/v1/palettes', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-      palette_name: paletteName,
-      project_id: parseInt(projectId),
-      ...paletteState.palette})
-  });
-  getPalettes();
+  if(!$(projectTitle).find('h3').text().includes(paletteName)) {
+    await fetch('/api/v1/palettes', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        palette_name: paletteName,
+        project_id: parseInt(projectId),
+        ...paletteState.palette})
+    });
+    getPalettes();
+  } else {
+    alert('Palette Names Within Each Project Must Be Unique!')
+  }
 });
 
 const deletePalette = async (e) => {
@@ -102,14 +111,12 @@ $('#project-container').on('click', '.delete', deletePalette);
 const getProjects = async (title) => {
   const initialFetch = await fetch('/api/v1/projects');
   const projects = await initialFetch.json();
-  console.log('projects', projects)
   projects.forEach(project => createProjects(project));
 }
 
 const createProjects = (project) => {
-  console.log('project', project)
   $('#select-options').append(`<option value=${project.id}>${project.title}</option>`);
-  $('#project-container').append(`<h2 id=${project.id} style='display:none'>${project.title}</h2>`)
+  $('#project-container').append(`<h2 class=${project.title} id=${project.id} style='display:none'>${project.title}</h2>`)
 }
 
 const getPalettes = async () => {
@@ -119,10 +126,9 @@ const getPalettes = async () => {
 }
 
 const createPalettes = (palette) => {
-  console.log('palette', palette)
   const addColors = createColors(palette);
   $('#' + palette.project_id).css('display', 'inline');
-  $('#' + palette.project_id).append(`<div>
+  $('#' + palette.project_id).append(`<div id="saved-palette">
                                         <h3 id=${palette.id}>
                                           ${palette.palette_name}
                                         </h3>
@@ -139,7 +145,7 @@ const createColors = (palette) => {
   let result = '';
   for(let i = 1; i <= 5; i++) {
     if(palette) {
-      result += `<div style="background-color:${palette[`color${i}`]}">${palette[`color${i}`]}</div>`
+      result += `<div id="color" style="background-color:${palette[`color${i}`]}">${palette[`color${i}`]}</div>`
     }    
   }
   return result;
